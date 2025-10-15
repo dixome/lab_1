fe219c5110c2680ff07d2bebe9d5da0af326124b
Мыльникова Диана <dinaxome@gmail.com>
2025-10-15 13:29:37 +0000

    add_pattern

--- src/main.py
+++ src/main.py
@@ -22,3 +22,27 @@ for y in range(HEIGHT):
         else:
             print(GREEN + " ", end="")
     print(END)
+
+print('задание 2: повторяющийся узор')	
+
+BLACK = '\u001b[40m'
+WHITE = '\u001b[47m'
+END = '\u001b[0m'
+
+WIDTH = 11
+HEIGHT = 7
+repeats = 5
+
+for y in range(HEIGHT):
+    for r in range(repeats):
+        for x in range(WIDTH):
+            if (y in [0, 6] or
+                (y == 1 and x > 9) or
+                (y == 2 and (1 < x < 8 or x > 9)) or 
+                (y == 3 and (1 < x < 4 or x > 9)) or
+                (y == 4 and (1 < x < 4 or x > 5)) or
+                (y == 5 and 1 < x < 4)):
+                print(f"{BLACK} {END}", end="")
+            else:
+                print(f"{WHITE} {END}", end="")
+    print()
